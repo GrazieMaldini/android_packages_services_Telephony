a7cdd10ebd3ecbe30fadabaa5b3be2ce35b8ef92
sayan7848 <sayan7848@gmail.com>
2018-12-24 20:19:01 +0000

    Revert "Show or hide call forwarding,additional settings according to carrier config."
    This reverts commit f6fd992fe7adb530013f5d28d0bd32aa401ad4d4.
    
    Signed-off-by: sayan7848 <sayan7848@gmail.com>

--- src/com/android/phone/GsmUmtsAdditionalCallOptions.java
+++ src/com/android/phone/GsmUmtsAdditionalCallOptions.java
@@ -2,10 +2,8 @@ package com.android.phone;
 
 import android.app.ActionBar;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
-import android.telephony.CarrierConfigManager;
 import android.util.Log;
 import android.view.MenuItem;
 
@@ -28,9 +26,6 @@ public class GsmUmtsAdditionalCallOptions extends TimeConsumingPreferenceActivit
     private Phone mPhone;
     private SubscriptionInfoHelper mSubscriptionInfoHelper;
 
-    private boolean mShowCLIRButton;
-    private boolean mShowCWButton;
-
     @Override
     protected void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -46,60 +41,24 @@ public class GsmUmtsAdditionalCallOptions extends TimeConsumingPreferenceActivit
         mCLIRButton = (CLIRListPreference) prefSet.findPreference(BUTTON_CLIR_KEY);
         mCWButton = (CallWaitingSwitchPreference) prefSet.findPreference(BUTTON_CW_KEY);
 
-        PersistableBundle b = null;
-        if (mSubscriptionInfoHelper.hasSubId()) {
-            b = PhoneGlobals.getInstance().getCarrierConfigForSubId(
-                    mSubscriptionInfoHelper.getSubId());
+        mPreferences.add(mCLIRButton);
+        mPreferences.add(mCWButton);
+
+        if (icicle == null) {
+            if (DBG) Log.d(LOG_TAG, "start to init ");
+            mCLIRButton.init(this, false, mPhone);
         } else {
-            b = PhoneGlobals.getInstance().getCarrierConfig();
-        }
-
-        if (b != null) {
-            mShowCLIRButton = b.getBoolean(
-                    CarrierConfigManager.KEY_ADDITIONAL_SETTINGS_CALLER_ID_VISIBILITY_BOOL);
-            mShowCWButton = b.getBoolean(
-                    CarrierConfigManager.KEY_ADDITIONAL_SETTINGS_CALL_WAITING_VISIBILITY_BOOL);
-        }
-
-        if (mCLIRButton != null) {
-            if (mShowCLIRButton) {
-                mPreferences.add(mCLIRButton);
+            if (DBG) Log.d(LOG_TAG, "restore stored states");
+            mInitIndex = mPreferences.size();
+            mCLIRButton.init(this, true, mPhone);
+            mCWButton.init(this, true, mPhone);
+            int[] clirArray = icicle.getIntArray(mCLIRButton.getKey());
+            if (clirArray != null) {
+                if (DBG) Log.d(LOG_TAG, "onCreate:  clirArray[0]="
+                        + clirArray[0] + ", clirArray[1]=" + clirArray[1]);
+                mCLIRButton.handleGetCLIRResult(clirArray);
             } else {
-                prefSet.removePreference(mCLIRButton);
-            }
-        }
-
-        if (mCWButton != null) {
-            if (mShowCWButton) {
-                mPreferences.add(mCWButton);
-            } else {
-                prefSet.removePreference(mCWButton);
-            }
-        }
-
-        if (mPreferences.size() != 0) {
-            if (icicle == null) {
-                if (DBG) Log.d(LOG_TAG, "start to init ");
-                doPreferenceInit(mInitIndex);
-            } else {
-                if (DBG) Log.d(LOG_TAG, "restore stored states");
-                mInitIndex = mPreferences.size();
-                if (mShowCWButton) {
-                    mCWButton.init(this, true, mPhone);
-                }
-                if (mShowCLIRButton) {
-                    mCLIRButton.init(this, true, mPhone);
-                    int[] clirArray = icicle.getIntArray(mCLIRButton.getKey());
-                    if (clirArray != null) {
-                        if (DBG) {
-                            Log.d(LOG_TAG, "onCreate:  clirArray[0]="
-                                    + clirArray[0] + ", clirArray[1]=" + clirArray[1]);
-                        }
-                        mCLIRButton.handleGetCLIRResult(clirArray);
-                    } else {
-                        mCLIRButton.init(this, false, mPhone);
-                    }
-                }
+                mCLIRButton.init(this, false, mPhone);
             }
         }
 
@@ -114,7 +73,7 @@ public class GsmUmtsAdditionalCallOptions extends TimeConsumingPreferenceActivit
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
 
-        if (mShowCLIRButton && mCLIRButton.clirArray != null) {
+        if (mCLIRButton.clirArray != null) {
             outState.putIntArray(mCLIRButton.getKey(), mCLIRButton.clirArray);
         }
     }
@@ -123,7 +82,10 @@ public class GsmUmtsAdditionalCallOptions extends TimeConsumingPreferenceActivit
     public void onFinished(Preference preference, boolean reading) {
         if (mInitIndex < mPreferences.size()-1 && !isFinishing()) {
             mInitIndex++;
-            doPreferenceInit(mInitIndex);
+            Preference pref = mPreferences.get(mInitIndex);
+            if (pref instanceof CallWaitingSwitchPreference) {
+                ((CallWaitingSwitchPreference) pref).init(this, false, mPhone);
+            }
         }
         super.onFinished(preference, reading);
     }
@@ -136,16 +98,5 @@ public class GsmUmtsAdditionalCallOptions extends TimeConsumingPreferenceActivit
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    private void doPreferenceInit(int index) {
-        if (mPreferences.size() != 0) {
-            Preference pref = mPreferences.get(index);
-            if (pref instanceof CallWaitingSwitchPreference) {
-                ((CallWaitingSwitchPreference) pref).init(this, false, mPhone);
-            } else if (pref instanceof CLIRListPreference) {
-                ((CLIRListPreference) pref).init(this, false, mPhone);
-            }
-        }
     }
 }
